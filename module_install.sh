538cb3d12f0889b58ed4276f054064fe369d5ea0
crue-ton <83989081+crue-ton@users.noreply.github.com>
2021-05-10 19:35:34 -0400

    for a easier install for the modules

---
+++ module_install.sh
@@ -0,0 +1,5 @@
+echo Installing your modules!
+py -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+echo Modules done installing!
