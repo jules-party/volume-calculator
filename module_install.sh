516c5ee98037b73f505f96fe2ea8dc1ba70d26b7
crue-ton <83989081+crue-ton@users.noreply.github.com>
2021-05-10 21:08:03 -0400

    Delete module_install.sh

--- module_install.sh
+++
@@ -1,5 +0,0 @@
-echo Installing your modules!
-py -m pip install -r requirements.txt
-python -m pip install -r requirements.txt
-python3 -m pip install -r requirements.txt
-echo Modules done installing!
